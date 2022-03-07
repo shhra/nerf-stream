c50ea9adc126149205c1ea3e014c4a48c9ec2009
shhra <29188967+shhra@users.noreply.github.com>
2022-03-07 23:02:43 +0100

    Add a lot of buggy code.
    This update should have the main nerf logic. However, the code is still
    buggy. Therefore, it requires a lot of fine-tuning.

--- nerf.py
+++ nerf.py
@@ -7,8 +7,6 @@ np.random.seed(3032022)
 
 # To download the data use follow command:
 # wget http://cseweb.ucsd.edu/\~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
-
-
 # STREAM PART 1
 def load_and_view_data(visualize=False):
     data = np.load("tiny_nerf_data.npz", allow_pickle=False)
@@ -31,7 +29,7 @@ def pose_encoder(x, hp_L=5):
     for i in range(hp_L):
         noise.append(np.cos(2 ** i * np.pi * x + 10 * i * np.pi))
         noise.append(np.sin(2 ** i * np.pi * x + 10 * i * np.pi))
-    return np.array(noise)
+    return torch.from_numpy(np.array(noise)).flatten()
 
 
 class Nerf(torch.nn.Module):
@@ -97,13 +95,53 @@ def get_ray(width, height, focal_length, camera_to_world):
 
 def render(near, far, num_samples, ray_origin, ray_direction, nerf):
     # Sample rays from near to far.
-    pts = torch.linspace(near, far)
-    pts += torch.rand((ray_origin.shape[:-1], num_samples))
-    # Sampling new hit points for volumetric rendering
-    sample_pts = ray_origin[..., None,:] + ray_direction[..., None, :] * pts[..., :, None]
-    # Find the radiance and volume density
-    #
+    pts = torch.linspace(near, far, num_samples)
+    # Sample one random point, scale it and and add it to the current point. This randomizations
+    # offsets the point to somewhere between near pt and pt + 1
+    pts += torch.rand((ray_origin.shape[:-1], num_samples)) * (far - near) / num_samples
 
+    # Stream part 3.
+    # Sampling new hit points for density and radiance.
+    sample_pts = (
+        ray_origin[..., None, :] + ray_direction[..., None, :] * pts[..., :, None]
+    )
+    samples = sample_pts.view(-1, 3)
+    pos = pose_encoder(samples, 5)  # Paper uses 10.
+    dirs = pose_encoder(sample_pts, 4)
+    density, radiance = nerf(dirs, pos)
+
+    # Volume rendering
+    dists = pts[..., 1:] - pts[..., :-1]
+    alpha = 1 - torch.exp(-density * dists)
+    weights = alpha * torch.cumprod(1.0 - alpha + 1e-10, -1)
+    rgb_color = torch.sum(weights * radiance, -2)
+
+    return rgb_color
+
+
+def train(images, poses, focal, height, width):
+    num_samples = 4
+    num_iters = 1000
+    psnr = []
+    nerf = Nerf()
+
+    optimizer =  torch.optim.Adam(nerf.parameters(), lr=1e-5)
+
+    for i in range(num_iters + 1):
+        rnd_i = np.random.randint(images.shape[0])
+        target = images[rnd_i]
+        pose = poses[rnd_i]
+
+        optimizer.zero_grad()
+        ray_o, ray_d = get_ray(width, height, focal, pose)
+        rgb = render(2.0, 6.0, num_samples, ray_o, ray_d, nerf)
+        loss = torch.nn.MSELoss()(rgb, target)
+        loss.backward()
+        optimizer.step()
+
+        if i % 100 == 0:
+            print("Printing stats:\n Step: {}".format(i))
+            # TODO: Use the test data to plot stats
 
 def test():
     image, poses, focal = load_and_view_data()
